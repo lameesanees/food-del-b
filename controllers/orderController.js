2d01b7b15b4f4e4589c42574b09e1749d1c9cdc1
Lamees Anees <lamees.anees@gmail.com>
2024-06-15 14:04:41 +0530

    fve

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://foodexpressify.netlify.app";
+  const frontend_url = "http://localhost:5173";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
